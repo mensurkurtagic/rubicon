b16b453741e7d1bf8fa3b8384d077fda46f56d29
Mensur Kurtagic <mensur.kurtagic@yahoo.com>
2018-12-15 17:10:36 +0100

    first commit

---
+++ rubicon/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Rubicon App</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="toggle">
+            <div class="toggle-movies">
+                <span class="movie-title">Movies</span>
+            </div>
+
+            <div class="toggle-shows">
+                <span class="shows-title">TV Shows</span>
+            </div>
+        </div>
+
+        <div class="search">
+            <input type="text" class="" placeholder="Search your movies/shows">
+        </div>
+    </div>
+</body>
+</html>
